4a22fec9e7d8483bc825915e8f137da9f3496c05
dgkncelik <dogukan.celik@itu.edu.tr>
2019-12-29 23:30:18 +0300

    Implement model-field relationship

--- model/__init__.py
+++ model/__init__.py
@@ -5,6 +5,11 @@ A 'Model' has 'Fields' which are key-value based object attributes
 from storage import Storage
 from message import Publisher
 from message import Consumer
+from model.field import Field
+
+
+class ModelException(Exception):
+    pass
 
 
 class Model(object):
@@ -13,35 +18,57 @@ class Model(object):
         self.__publisher = publisher
         self.__consumer = consumer
 
-        if data is None:
+        if data is not None and not isinstance(data, dict):
+            raise ModelException('data must be dictionary')
+        elif data is None:
             self.__data = {}
-        else:
-            self.__data = data
 
         self.__arguments = args
         self.__parameters = kwargs
         self.__fields = None
 
+    def __getattribute__(self, item):
+        all_attributes = dir(self)
+        if item in all_attributes:
+            attr = getattr(self, item)
+            if isinstance(attr, Field):
+                return attr.value
+            else:
+                return getattr(self, item)
+        else:
+            getattr(self, item)
+
+    def __setattr__(self, key, value):
+        all_attributes = dir(self)
+        if key in all_attributes:
+            attr = getattr(self, key)
+            if isinstance(attr, Field):
+                attr.value = value
+            else:
+                setattr(self, key, value)
+        else:
+            setattr(self, key, value)
+
     def validate(self):
         pass
 
     def save(self):
-        pass
+        self.__storage.save(self)
 
     def update(self):
-        pass
+        self.__storage.save(self)
 
     def delete(self):
-        pass
+        self.__storage.delete(self)
 
     def filter(self):
-        pass
+        self.__storage.find(self)
 
     def send(self):
-        pass
+        self.__publisher.publish(self)
 
     def receive(self):
-        pass
+        self.__consumer.get(self)
 
     def data(self):
-        pass
+        return self.__data
